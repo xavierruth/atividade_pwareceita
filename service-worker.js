91cd7c61330fb870e1a0cd41527d6909f56ffac2
xavierruth <ruthmarianne.xavier@gmail.com>
2024-03-30 01:55:26 -0300

    att

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,36 @@
-var cacheName = 'TURecetas+-v1.0';
+var cacheName = 'TURecetas-v1.0';
 
 self.addEventListener('install', event => {
-
   self.skipWaiting();
-
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll([
-
-        './index.html',
-
-        './assets/icons',
-
-        './assets/icons/apple.svg',
-
-        './assets/icons/facebook.svg',
-
-        './assets/icons/google.svg',
-
-        './assets/icons/apple.svg',
-
-        './assets/img_user/logonome.svg',
-
-        './assets/img_user/fotousuario.svg',
-
-        './scripts/forms.js',
-
-        './style/style.css',
-
-        './style/login.css',
-
-        './style/reset.css',
-
-        './home.html', 
-
-        './login.html',
-
-
-        './assets/img/29.png',
-        './assets/img/40.png',
-        './assets/img/57.png',
-        './assets/img/58.png',
-        './assets/img/60.png',
-        './assets/img/80.png',
-        './assets/img/87.png',
-        './assets/img/114.png',
-        './assets/img/120.png',
-        './assets/img/180.png',
-        './assets/img/512.png',
-        './assets/img/1024.png',
-
-      ]))
+    caches.open(cacheName).then(cache => cache.addAll([
+      './index.html',
+      './assets/icons',
+      './assets/icons/apple.svg',
+      './assets/icons/facebook.svg',
+      './assets/icons/google.svg',
+      './assets/icons/apple.svg',
+      './assets/img_user/logonome.svg',
+      './assets/img_user/fotousuario.svg',
+      './scripts/forms.js',
+      './styles/style.css',
+      './styles/login.css',
+      './styles/reset.css',
+      './home.html',
+      './login.html',
+      './assets/img/29.png',
+      './assets/img/40.png',
+      './assets/img/57.png',
+      './assets/img/58.png',
+      './assets/img/60.png',
+      './assets/img/80.png',
+      './assets/img/87.png',
+      './assets/img/114.png',
+      './assets/img/120.png',
+      './assets/img/180.png',
+      './assets/img/512.png',
+      './assets/img/1024.png',
+    ]))
   );
 });
 
@@ -61,7 +41,6 @@ self.addEventListener('message', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  //Atualizacao internet
   event.respondWith(async function () {
     try {
       return await fetch(event.request);
@@ -69,16 +48,4 @@ self.addEventListener('fetch', function (event) {
       return caches.match(event.request);
     }
   }());
-
-  //Atualizacao cache
-  /*event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );*/
-
 });
